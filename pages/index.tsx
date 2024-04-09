a2c50b0b80d7f3d896613e3bbe1e10d8a93af806
michaelimmanuel <57061376+michaelimmanuel@users.noreply.github.com>
2024-04-09 14:38:02 +0700

    added prisma dependency
    added prisma dependency for backend support

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,15 @@ import HeaderLoggedIn1 from "../components/header-logged-in1";
 import Footer from "../components/footer";
 
 import styles from "./index.module.css";
+import React from "react";
 
 const TutorsList: NextPage = () => {
 
   return (
     <div className={styles.tutorsList}>
       <div className={styles.tutorsListChild} />
-      <HeaderLoggedIn1 />
-      <Footer />
+      <HeaderLoggedIn1/>
+      <Footer/>
 
     </div>
   );
