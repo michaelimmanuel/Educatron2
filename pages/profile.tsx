e9f88dae3984d433a6ee7278d40df753ca3f70b6
MrJayMerZzz <jameswidjajaedwin@gmail.com>
2024-04-27 19:45:26 +0700

    Register + Profile
    Register + Profile (Unfinished) + Icon Password

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,62 +1,19 @@
 import type { NextPage } from "next";
 import FrameComponent from "../components/frame-component";
 import InfoIcon from "../components/info-icon";
+import Profile from "../components/profile"; 
 import Footer from "../components/footer";
 import styles from "../src/output.css";
 import 'tailwindcss/tailwind.css';
 import 'daisyui/dist/full.css';
 
-const Profile: NextPage = () => {
+
+const Profile1: NextPage = () => {
   return (
-    <div className={styles.profile}>
-      <div className={styles.profileChild} />
-      <FrameComponent />
-      <main className={styles.profileInner}>
-        <section className={styles.policyLinkWrapper}>
-          <div className={styles.policyLink}>
-            <div className={styles.externalWrapper}>
-              <div className={styles.external}>
-                <div className={styles.logo}>
-                  <img
-                    className={styles.profilePicIcon}
-                    alt=""
-                    src="/profile-pic-1@2x.png"
-                  />
-                  <div className={styles.edwardNathanSamuelParent}>
-                    <h1 className={styles.edwardNathanSamuel}>
-                      Edward Nathan Samuel
-                    </h1>
-                    <div className={styles.id}>#696969420ID</div>
-                  </div>
-                </div>
-                <div className={styles.navigation}>
-                  <div className={styles.profileWrapper}>
-                    <h1 className={styles.edwardNathanSamuel}>Profile</h1>
-                  </div>
-                  <div className={styles.profileWrapper}>
-                    <h1 className={styles.edwardNathanSamuel}>Security</h1>
-                  </div>
-                  <div className={styles.profileFrame}>
-                    <h1
-                      className={styles.edwardNathanSamuel}
-                    >{`Payments & History`}</h1>
-                  </div>
-                  <div className={styles.frameDiv}>
-                    <h1 className={styles.edwardNathanSamuel}>Account</h1>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className={styles.rectangleParent}>
-              <div className={styles.frameChild} />
-              <InfoIcon />
-            </div>
-          </div>
-        </section>
-      </main>
-      <Footer />
+    <div>
+      <Profile /> 
     </div>
   );
 };
 
-export default Profile;
+export default Profile1;
